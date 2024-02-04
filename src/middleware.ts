c62633062ce36400eeac47151ee0a838b1530439
Aadil Matloob <8232aadil@gmail.com>
2024-02-04 13:25:16 +0530

    feat: middleware to rewrite and redirect requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,55 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/site", "/api/uploadThing"],
+  async beforeAuth(auth, req) {},
+  async afterAuth(auth, req) {
+    // rewrite for domains
+    const url = req.nextUrl;
+    const searchParams = url.searchParams.toString();
+
+    let hostname = req.headers;
+
+    console.log("URL PATHNAME", url.pathname);
+    console.log("URL HOSTNAME", hostname);
+    console.log("URL SEARCH PARAMS", searchParams);
+
+    const pathWithSearchParams = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ""
+    }`;
+
+    // if subdomains are used, we need to rewrite the url to the correct domain
+    const customSubDomain = hostname
+      .get("host")
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0];
+
+    console.log("CUSTOM SUBDOMAIN", customSubDomain);
+
+    if (customSubDomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+      );
+    }
+
+    if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
+    }
+    if (
+      (url.pathname === "/" || url.pathname === "/site") &&
+      url.host === process.env.NEXT_PUBLIC_DOMAIN
+    ) {
+      return NextResponse.rewrite(new URL(`/site`, req.url));
+    }
+
+    if (
+      url.pathname.startsWith("/agency") ||
+      url.pathname.startsWith("subaccount")
+    ) {
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url));
+    }
+  },
 });
 
 export const config = {
