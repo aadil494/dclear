33a6f906642bf252f06df0f329cc934e152a3c7f
Aadil Matloob <8232aadil@gmail.com>
2024-02-06 23:57:51 +0530

    feat : save the user on invitation

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -2,6 +2,8 @@
 
 import { currentUser } from "@clerk/nextjs";
 import { db } from "./db";
+import { redirect } from "next/navigation";
+import { User } from "@prisma/client";
 export const getAuthUserDetails = async () => {
   const user = await currentUser();
   if (!user) {
@@ -27,4 +29,35 @@ export const getAuthUserDetails = async () => {
     },
   });
   return userData;
+};
+
+export const createTeamUser = async (agencyId: string, user: User) => {
+  if (user.role === "AGENCY_OWNER") return null;
+  const response = await db.user.create({ data: { ...user } });
+  return response;
+};
+
+export const veriyAndAcceptInvitation = async () => {
+  const user = await currentUser();
+  if (!user) return redirect("/sign-in");
+
+  const invitationExists = await db.invitation.findUnique({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+      status: "PENDING",
+    },
+  });
+
+  if (invitationExists) {
+    const userDetails = await createTeamUser(invitationExists.agencyId, {
+      email: invitationExists.email,
+      agencyId: invitationExists.agencyId,
+      avatarUrl: user.imageUrl,
+      id: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      role: invitationExists.role,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
 };
