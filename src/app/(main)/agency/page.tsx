67c0c69adc00f6d2691f0376eaf5031a053016c7
Aadil Matloob <8232aadil@gmail.com>
2024-02-07 23:23:36 +0530

    gen: finalize the invitation logic

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -2,13 +2,13 @@ import { getAuthUserDetails, veriyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
-import { current } from "tailwindcss/colors";
 
 const Page = async () => {
   const authUser = await currentUser();
   if (!authUser) return redirect("/sign-in");
 
   const agencyId = await veriyAndAcceptInvitation();
+  console.log(agencyId, "agencyId");
 
   // get user details
   const user = getAuthUserDetails();
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,9 +1,12 @@
 "use server";
 
-import { currentUser } from "@clerk/nextjs";
+import { clerkClient, currentUser } from "@clerk/nextjs";
 import { db } from "./db";
 import { redirect } from "next/navigation";
 import { User } from "@prisma/client";
+import { sub } from "date-fns";
+import { Sub } from "@radix-ui/react-dropdown-menu";
+
 export const getAuthUserDetails = async () => {
   const user = await currentUser();
   if (!user) {
@@ -42,7 +45,81 @@ export const saveActivityLogsNotification = async ({
   const authUser = await currentUser();
   let userData;
   if (!authUser) {
-    const response = await db.user.findFirst({});
+    const response = await db.user.findFirst({
+      where: {
+        Agency: {
+          SubAccount: {
+            some: { id: subAccountId },
+          },
+        },
+      },
+    });
+    if (response) {
+      userData = response;
+    }
+  } else {
+    userData = await db.user.findUnique({
+      where: {
+        email: authUser.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+  if (!userData) {
+    console.log("User not found");
+    return;
+  }
+
+  let foundAgencyId = agencyId;
+  if (!foundAgencyId) {
+    if (!subAccountId) {
+      throw new Error("Agency id or sub account id is required");
+    }
+    const response = await db.subAccount.findUnique({
+      where: {
+        id: subAccountId,
+      },
+    });
+    if (response) {
+      foundAgencyId = response.agencyId;
+    }
+    if (subAccountId) {
+      await db.notification.create({
+        data: {
+          notification: `${userData.name} ${description}`,
+          User: {
+            connect: {
+              id: userData.id,
+            },
+          },
+          Agency: {
+            connect: {
+              id: foundAgencyId,
+            },
+          },
+          SubAccount: {
+            connect: {
+              id: subAccountId,
+            },
+          },
+        },
+      });
+    } else {
+      await db.notification.create({
+        data: {
+          notification: `${userData.name} ${description}`,
+          User: {
+            connect: {
+              id: userData.id,
+            },
+          },
+          Agency: {
+            connect: {
+              id: foundAgencyId,
+            },
+          },
+        },
+      });
+    }
   }
 };
 
@@ -74,5 +151,36 @@ export const veriyAndAcceptInvitation = async () => {
       createdAt: new Date(),
       updatedAt: new Date(),
     });
+
+    // activity logs : when user does any action in the app then we need to save the logs
+    await saveActivityLogsNotification({
+      description: "Joined",
+      agencyId: invitationExists?.agencyId,
+    });
+
+    if (userDetails) {
+      await clerkClient.users.updateUserMetadata(user.id, {
+        privateMetadata: {
+          role: userDetails.role || "SUBACCOUNT_USER",
+        },
+      });
+
+      // delete the invitation
+      await db.invitation.delete({
+        where: {
+          email: userDetails.email,
+        },
+      });
+      return userDetails;
+    } else {
+      return null;
+    }
+  } else {
+    const agency = await db.user.findUnique({
+      where: {
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+    return agency ? agency.agencyId : null;
   }
 };
