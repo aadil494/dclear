92e056051eae699e550b14b973b29bae04c3830f
Aadil Matloob <8232aadil@gmail.com>
2024-02-13 00:57:22 +0530

    feat: finish agency details form

--- src/components/forms/AgencyDetailsForm.tsx
+++ src/components/forms/AgencyDetailsForm.tsx
@@ -27,11 +27,26 @@ import { Input } from "../ui/input";
 import { Switch } from "../ui/switch";
 import { NumberInput } from "@tremor/react";
 import {
+  deleteAgency,
+  initUser,
   saveActivityLogsNotification,
   updateAgencyDetails,
+  upsertAgency,
 } from "@/lib/queries";
 import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
+import Loading from "../global/loading";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { v4 } from "uuid";
 type Props = {
   data?: Partial<Agency>;
 };
@@ -95,7 +110,90 @@ const AgencyDetailsForm = ({ data }: Props) => {
   }, [data]);
 
   const isLoading = form.formState.isSubmitting;
-  const handleSubmit = async () => {};
+  const handleSubmit = async (values: z.infer<typeof FormSchema>) => {
+    try {
+      let newUserDta;
+      let customerId;
+      if (!data?.id) {
+        const bodyData = {
+          email: values.companyEmail,
+          name: values.name,
+          phone: values.companyPhone,
+          shipping: {
+            address: {
+              city: values.city,
+              country: values.country,
+              line1: values.address,
+              postal_code: values.zipCode,
+              state: values.state,
+            },
+            name: values.name,
+          },
+          address: {
+            city: values.city,
+            country: values.country,
+            line1: values.address,
+            postal_code: values.zipCode,
+            state: values.state,
+          },
+        };
+      }
+
+      newUserDta = await initUser({ role: "AGENCY_OWNER" });
+      if (!data?.id) {
+        const response = await upsertAgency({
+          id: data?.id ? data.id : v4(),
+          address: values.address,
+          agencyLogo: values.agencyLogo,
+          city: values.city,
+          companyPhone: values.companyPhone,
+          country: values.country,
+          name: values.name,
+          state: values.state,
+          whiteLabel: values.whiteLabel,
+          zipCode: values.zipCode,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          companyEmail: values.companyEmail,
+          connectAccountId: "",
+          goal: 5,
+        });
+
+        toast({
+          title: "Created Agency",
+        });
+        if (data?.id) return router.refresh();
+        if (response) {
+          return router.refresh();
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: "Error",
+        description: "Something went wrong. Please try again",
+      });
+    }
+  };
+
+  const handleDeleteAgency = async () => {
+    if (!data?.id) return;
+    setDeletingAgency(true);
+    try {
+      const response = await deleteAgency(data.id);
+      toast({
+        title: "Agency Deleted",
+        description: "The agency and Sub Accounts have been deleted",
+      });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong. Please try again",
+      });
+      setDeletingAgency(false);
+    }
+  };
 
   return (
     <AlertDialog>
@@ -280,7 +378,7 @@ const AgencyDetailsForm = ({ data }: Props) => {
                   )}
                 />
 
-                {true && (
+                {data?.id && (
                   <div className="flex flex-col gap-2">
                     <FormLabel>Create A Goal</FormLabel>
                     <FormDescription>
@@ -305,9 +403,54 @@ const AgencyDetailsForm = ({ data }: Props) => {
                     />
                   </div>
                 )}
-                <Button type="submit">Save Agency Information</Button>
+                <Button type="submit">
+                  {isLoading ? <Loading /> : "Save Agency Details"}
+                </Button>
               </form>
             </Form>
+            {data?.id && (
+              <div>
+                <div className="flex items-center flex-row justify-between rounded-lg border border-destructive gap-4 p-4 mt-4">
+                  <div>
+                    <div>Danger Zone</div>
+                    <div className="text-muted-foreground">
+                      Deleting an agency will delete all sub accounts and their
+                      data. This action is irreversible. Sub accounts will no
+                      longer have access to funnels, automations, and other
+                      data.
+                    </div>
+                  </div>
+                </div>
+
+                <AlertDialogTrigger
+                  disabled={isLoading || deletingAgency}
+                  className="text-red-600 p-2 text-center mt-2 rounded-md hover:bg-red-600 hover:text-white whitespace-nowrap"
+                >
+                  {deletingAgency ? "Deleting..." : "Delete Agency"}
+                </AlertDialogTrigger>
+              </div>
+            )}
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle className="text-left">
+                  Are you absolutely sure?
+                </AlertDialogTitle>
+                <AlertDialogDescription className="text-left">
+                  This action cannot be undone. This will permanently delete the
+                  Agency account and all related sub accounts.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter className="flex items-center">
+                <AlertDialogCancel className="mb-2">Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  disabled={deletingAgency}
+                  className="bg-destructive hover:bg-destructive"
+                  onClick={handleDeleteAgency}
+                >
+                  Delete
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
           </CardContent>
         </CardHeader>
       </Card>
